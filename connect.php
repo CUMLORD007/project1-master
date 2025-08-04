f080c009eebd5cbcf99ef2b21e6ca60a1b8e0075
CUMLORD007 <num.nue.com@gmail.com>
2025-08-04 08:29:39 +0700

    one commit

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+
+if(session_status() === PHP_SESSION_NONE){
+    session_start();
+}
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "users"; 
+
+$con = mysqli_connect($host,$user,$pass,$dbname);
+if (mysqli_connect_errno()){
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
